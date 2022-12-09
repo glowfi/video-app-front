b1c3fc6487d302abdf79bd56c0927aba4abbcfb7
glowfi <86939591+glowfi@users.noreply.github.com>
2022-12-09 21:55:33 +0530

    added minor fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-    baseURL: 'https://utube-video-app.herokuapp.com/api/'
+    baseURL: 'https://video-app-back.onrender.com/api/'
 });
